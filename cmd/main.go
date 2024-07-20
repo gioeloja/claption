34ddf21e9663176c66c2f5bdbfc10a211aee6e27
Gio B Eloja <gio.b.eloja@gmail.com>
2024-07-20 15:16:36 -0500

    s3 bucket can be made now

--- cmd/main.go
+++ cmd/main.go
@@ -17,9 +17,11 @@ func main() {
 
 	// Initialize AWS session
 	sess, err := session.NewSession(&aws.Config{
-		Region:   aws.String(os.Getenv("AWS_REGION")),
-		Endpoint: aws.String(os.Getenv("LOCALSTACK_ENDPOINT")),
+		Region:           aws.String(os.Getenv("AWS_REGION")),
+		Endpoint:         aws.String(os.Getenv("LOCALSTACK_ENDPOINT")),
+		S3ForcePathStyle: aws.Bool(true), // needed for localstack to work
 	})
+
 	if err != nil {
 		fmt.Printf("Failed to initialize new session: %v", err)
 		return
@@ -28,7 +30,7 @@ func main() {
 	// Initialize S3 service
 	s3Client := s3.New(sess)
 
-	bucketName := "testBucket"
+	bucketName := "test-bucket"
 	err = pkg.CreateBucket(s3Client, bucketName)
 	if err != nil {
 		fmt.Printf("Couldn't create bucket: %v", err)
