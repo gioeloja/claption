4e8fa940d919a32bb9a74590cd7ee0a9d4857422
Gio B Eloja <gio.b.eloja@gmail.com>
2024-07-19 20:47:06 -0500

    making aws client and s3 bucket

---
+++ cmd/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"caption_service/pkg"
+	"fmt"
+	"os"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	// Get env vars
+	godotenv.Load()
+
+	// Initialize AWS session
+	sess, err := session.NewSession(&aws.Config{
+		Region:   aws.String(os.Getenv("AWS_REGION")),
+		Endpoint: aws.String(os.Getenv("LOCALSTACK_ENDPOINT")),
+	})
+	if err != nil {
+		fmt.Printf("Failed to initialize new session: %v", err)
+		return
+	}
+
+	// Initialize S3 service
+	s3Client := s3.New(sess)
+
+	bucketName := "testBucket"
+	err = pkg.CreateBucket(s3Client, bucketName)
+	if err != nil {
+		fmt.Printf("Couldn't create bucket: %v", err)
+		return
+	}
+
+	fmt.Println("Successfully created bucket")
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module caption_service
 
 go 1.22.5
+
+require (
+	github.com/aws/aws-sdk-go v1.54.20 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+)
